48b240600c0effc874b2a21c8b5f7d5130e92e6f
upfield1129 <41042798+upfield1129@users.noreply.github.com>
2018-08-10 16:53:01 +0900

    Add files via upload

--- syuji2.py
+++ syuji2.py
@@ -202,13 +202,13 @@ def Run(ct,*arg):
                 rospy.sleep(1.0)
             x0[2]+=0.3
             ct.robot.MoveToX(x0,10.0,blocking=True)
-            rospy.sleep(1.0)
+            rospy.sleep(5.0)
 	    print "\n"
         
         q= [-0.02225494707637879, 0.027604753814144237, 0.02256845844164128, -2.2001560115435073, -0.00047772651727832574, 0.6569580325147487, 0.0010119170182285682]   #First position
         ct.robot.MoveToQ(q,10.0)
     
-        
+        #grip0.01
             
         
         
